cf87c421b2d31c01dd19e02bd6c9a07f1d2393e5
AlexJamesRitchie <alexjamesritchie@outlook.com>
2019-07-20 13:37:26 +1200

    Add new level art

---
+++ HotPotAto/Assets/Scripts/ScrollMaterialUV.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollMaterialUV : MonoBehaviour
+{
+
+    [SerializeField] private Material materialToScroll;
+    [SerializeField] private Vector2 scrollSpeed;
+    // Start is called before the first frame update
+    void Update()
+    {
+        Vector2 newOffset = materialToScroll.GetTextureOffset("_MainTex");
+        newOffset += scrollSpeed * Time.deltaTime;
+        materialToScroll.SetTextureOffset("_MainTex", newOffset);
+    }
+}
